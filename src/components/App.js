534666b3e4be1802b3beffffc837178893a439c1
Alison Golden <alisong779@yahoo.com>
2019-09-06 14:07:33 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,36 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (event) => {
+    this.setState({
+      filters: {
+        type: event.target.value
+      }
+    })
+  }
+
+  onFindPetsClick = () => {
+    let url
+
+    if (this.state.filters.type !== "all"){
+      url = `/api/pets?type=${this.state.filters.type}`
+    } else {
+      url = '/api/pets'
+    }
+    fetch(url)
+    .then(resp => resp.json())
+    .then(pets => {
+      this.setState({pets})
+    })
+  }
+
+  onAdoptPet = (id) => {
+    const pets = this.state.pets.map(p => {
+      return p.id === id ? {...p, isAdopted: true} : p
+    })
+    this.setState({pets})
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +54,13 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onChangeType={this.onChangeType}
+                onFindPetsClick={this.onFindPetsClick}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
@@ -37,3 +70,11 @@ class App extends React.Component {
 }
 
 export default App
+
+//App should pass a callback prop, onChangeType, to <Filters />
+//This callback needs to update <App />'s state.filters.type
+
+//Pass callback prop onFindPetsClick to <Filters />
+//When the <Filters /> component calls onFindPetsClick, <App /> should fetch a list of pets using fetch()
+
+//set <App/>'s state.pets with the results of your fetch request so you can pass the pet data down as props to <PetBrowser />
